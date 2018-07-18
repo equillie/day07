fb097322790c791021a81fcc662769b777760ce0
Eric QUILLIER <equillie@e2r9p14.42.fr>
2018-07-18 18:56:05 +0200

    Creation de ft_strdup.c

---
+++ ex00/ft_strdup.c
@@ -0,0 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strdup.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: equillie <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/07/18 14:19:19 by equillie          #+#    #+#             */
+/*   Updated: 2018/07/18 18:54:08 by equillie         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdlib.h>
+
+int		ft_strlen(char *str)
+{
+	int		i;
+
+	i = 0;
+	while (str[i] != '\0')
+		i++;
+	return (i);
+}
+
+char	*ft_strdup(char *src)
+{
+	char	*str;
+	int		i;
+
+	i = 0;
+	str = (char*)malloc(sizeof(src) * (ft_strlen(src) + 1));
+	while (i <= ft_strlen(src))
+	{
+		str[i] = src[i];
+		i++;
+	}
+	return (str);
+}
